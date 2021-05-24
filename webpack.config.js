d623010769dd05a02b3fadf9211fd50f4989aed5
A_D <ashishdubey@gmail.com>
2021-05-24 17:56:52 +0530

    setup webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,41 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
 
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_module/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        open: true,
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = () => {
+
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_module/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: 'inline-source-map',
+        devServer: {
+            open: true,
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 
 };
